23e36a6c62a6830a1f8b8fb149b24794a5ade9d9
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 22:09:26 +0200

    Constructor lecture

---
+++ ConsoleApplication6/Man.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Man
+    {
+
+        public string name;
+        public byte age;
+        public Man(string _name, byte _age)
+        {
+            name = _name;
+            age =  _age;
+        }
+        public void HappyBirthday()
+        {
+            Console.WriteLine ("Happy Birthday " + name);
+            age++;
+        }
+    }
+}
---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Name: ");
+            string n = Console.ReadLine();
+            Console.Write("Age: ");
+            byte age = Convert.ToByte( Console.ReadLine());
+            Man m1 = new Man(n, age);
+            m1.HappyBirthday();
+            m1.HappyBirthday();
+            Console.WriteLine(m1.name +" is "+ m1.age + " years old");
+
+            Man m2 = new Man("Alex", 30);
+           
+        }
+    }
+}
